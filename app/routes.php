ae2235bf6db61629fc2f09bcebab7ada2109b307
cmena <cmena@TRAN-4015-130.classic.local>
2015-01-06 14:10:21 +0100

    MAJ

--- app/routes.php
+++ app/routes.php
@@ -6,3 +6,9 @@ use Symfony\Component\HttpFoundation\Request;
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig');
 });
+
+// Details for a eleve
+$app->get('/eleves/{id}', function($id) use ($app) {
+    $eleve = $app['dao.eleve']->find($id);
+    return $app['twig']->render('eleve.html.twig', array('eleve' => $eleve));
+});
